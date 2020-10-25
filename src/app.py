f3607650da02961998ed7b944149900d23e74108
Ashish <ashm@stanford.edu>
2020-10-24 18:49:19 -0700

    first attempt at plot flask app

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, send_file
+from visualize_data import plot_var, do_plot
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/plot', methods=['GET'])
+def plot():
+    bytes_obj = do_plot("temperature", "C")
+
+    return send_file(bytes_obj,
+                     attachment_filename='plot.png',
+                     mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run()
--- src/visualize_data.py
+++ src/visualize_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import io
 
 DATA_FILE = '../data/reading_data_20201024_072314.csv'
 
@@ -18,18 +19,37 @@ variables = [#"timestamp",
              "pm1",
              "pm25",
              "pm10"]
+variable_units = [
+    ("temperature", "C"),
+    ("pressure", "hPa"),
+    ("humidity", "%"),
+    ("light", "Lux"),
+    ("oxidised", "kO"),
+    ("reduced", "kO"),
+    ("nh3", "kO"),
+    ("pm1", "ug/m3"),
+    ("pm25", "ug/m3"),
+    ("pm10", "ug/m3")
+]
 
-def plot_var(var, units = '', rolling_window_size=1):
-    x = pd.to_datetime(df['timestamp'],unit='s')
+def plot_var(var, units = '', rolling_window_size=1, show=True):
+    x = pd.to_datetime(df['timestamp'], unit='s')
     x = x.dt.tz_localize("GMT").dt.tz_convert('America/Los_Angeles').dt.tz_localize(None)
     y = df[var].rolling(window=rolling_window_size).mean()
     plt.plot(x,y)
-    # plt.xticks(x, rotation='vertical')
     plt.gcf().autofmt_xdate()
-    # plt.locator_params(axis='x', nbins=6)
     plt.title(f'{var} {units}')
-    plt.show()
+    if show:
+        plt.show()
 
+def do_plot(var, units='', rolling_window_size=1):
+    plot_var(var, units='', rolling_window_size=1, show=False)
+    # here is the trick save your figure into a bytes object and you can afterwards expose it via flas
+    bytes_image = io.BytesIO()
+    plt.savefig(bytes_image, format='png')
+    bytes_image.seek(0)
+    return bytes_image
 
-for var in variables:
-    plot_var(var, rolling_window_size=4)
+if __name__ == '__main__':
+    for var, unit in variable_units:
+        plot_var(var, units=unit, rolling_window_size=4)
